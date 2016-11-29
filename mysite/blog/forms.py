ac441df5870edc0dbfaf1ff72def75555f0b7748
vanstinger <vandykfernandes@hotmail.com>
2016-11-29 23:33:42 +0530

    Bug Fixes, Configured share it Form on the blog

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -3,6 +3,12 @@ from django import forms
 
 class EmailPostForm(forms.Form):
 	name = forms.CharField(max_length=25)
-	email = forms.EmailField()
-	to = forms.EmailField()
-	comments = forms.TextField(required=False)
+	email = forms.EmailField(label='Sender ID')
+	to = forms.EmailField(label='Receiver ID')
+	comments = forms.CharField(required=False, widget=forms.Textarea(
+		attrs={
+			"id": "comments",
+			"title": "Insert Comments",
+			"placeholder": "Place your comments here",
+		})
+	)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post
 from .forms import EmailPostForm
@@ -42,15 +43,27 @@ class PostListView(ListView):
 # Share Post View
 def post_share(request, post_id):
 	post = get_object_or_404(Post, id=post_id, status="published")
+	sent = False
+	receiver = ""
 
 	if request.method == "POST":
 		form = EmailPostForm(request.POST)
 		if form.is_valid():
 			cd = form.cleaned_data
+			receiver = cd['to']
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(
+				cd['name'], cd['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+				post.title, post_url, cd['name'], cd['comments'])
+			send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+			sent = True
 	else:
 		form = EmailPostForm()
 	context = {
 		'post': post,
 		'form': form,
+		'sent': sent,
+		'receiver': receiver,
 	}
 	return render(request, 'blog/share.html', context)
