21cdc1397873e4f613e9d5237f198a1dedf7108d
vanstinger <vandykfernandes@hotmail.com>
2016-12-05 23:03:25 +0530

    Custom filter in django template, Added post using markdown

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.db.models import Count
+# Custom Filters
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -21,3 +24,8 @@ def show_latest_posts(count=5):
 def get_most_commented_posts(count=5):
 	return Post.published.annotate(
 		total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+# Markdown Filter
+@register.filter(name='Markdown')
+def markdown_format(text):
+	return mark_safe(markdown.markdown(text))
