b896a8e81af9f669e819e058d85a9698e256fb15
vanstinger <vandykfernandes@hotmail.com>
2016-12-06 02:36:55 +0530

    Created sitemap to the site using django inbuilt module

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -37,6 +38,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Sitemaps
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    # My apps
     'blog',
     'taggit',
 )
