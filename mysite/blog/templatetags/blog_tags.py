467204540240311d4c2cdf75f644a9ba0eec5818
vanstinger <vandykfernandes@hotmail.com>
2016-12-05 20:54:57 +0530

    Custom Inclusion Tag for the blog template to display the Latest Blog Posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -4,6 +4,13 @@ register = template.Library()
 
 from ..models import Post
 
+# Simple Template tag to display total number of posts in the blog
 @register.simple_tag
 def total_posts():
 	return Post.published.count()
+
+# Inclusion Tag to display the Latest Post in the blog page
+@register.inclusion_tag('blog/latest_posts.html')
+def show_latest_posts(count=5):
+	latest_posts = Post.published.order_by('-publish')[:count]
+	return {'latest_posts': latest_posts}
